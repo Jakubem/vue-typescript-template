e27b8b06374e4f51e07d54c16156d72c1a72f576
Jakubem <jakubmx@gmail.com>
2018-02-19 23:29:40 +0100

    remove trailing whitespace

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -11,5 +11,4 @@ export class HomeComponent extends Vue {
   package: string = 'vue-ts-kie'
   repo: string = 'https://github.com/Jakubem/vue-typescript-template'
   mode: string = process.env.ENV
-
 }
